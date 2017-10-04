e03fd01db5c30cbb36271f868641f4ac6ff7a89a
hi_by <hi_by@ManuelsComputer>
2017-10-04 14:50:54 -0500

    Attempt 1.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,34 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/*
+ *
+ *
+ * @param String a
+ * @param String b
+ * @return String
+ *
+ */
+public class ReclamationProject {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
+    public static String doit(String a, String b) {
+        if (a.length() > b.length()) {
             String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+            a = b; b = c;
+        }
+
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
-}
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    r = (a.regionMatches(i, b, k, j)
+                            && j > r.length()) ? a.substring(i, i + j) : r; // Do it!
+                    }
+                } // Ah yeah
+            } return r;
+        }
+    }
+
